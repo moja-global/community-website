1d0ed533859b66dec6077c63b43bdba4efd6dbd4
sarthakkundra <sarthakkundra21@gmail.com>
2021-07-02 18:54:01 +0530

    Fixed Slack link
    Signed-off-by: sarthakkundra <sarthakkundra21@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -53,7 +53,7 @@ module.exports = {
           items: [
             {
               label: 'Slack',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              href: 'https://join.slack.com/t/mojaglobal/shared_invite/zt-o6ta1ug0-rVLjAo460~d7JbZ~HpFFtw',
             },
             {
               label: 'Official Website',
