b66dad41a6ef913fd193ac6d3f83c0d434c4ef3c
sarthakkundra <sarthakkundra21@gmail.com>
2021-07-03 14:21:03 +0530

    Created a generic card to be used for displaying info
    Signed-off-by: sarthakkundra <sarthakkundra21@gmail.com>

---
+++ website/src/components/ContributeCard/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './styles.css';
+
+const index = ({text, link}) => {
+    return (
+        <div className="card-container">
+            <div>
+                <h3>{text}</h3>
+                {link &&
+                <a>
+                    {link}
+                </a>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default index
