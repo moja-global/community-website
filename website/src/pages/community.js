2298388b8a946355aeb0f5b7d1e982d2d55dfa2f
sarthakkundra <sarthakkundra21@gmail.com>
2021-10-02 17:32:47 +0530

    added layout for community page

--- website/src/pages/community.js
+++ website/src/pages/community.js
@@ -1,12 +1,59 @@
-import React from 'react'
+import React from 'react';
 
 const Community = () => {
-    return (
-        <div>
-
-      <h1>Test</h1>
+  return (
+    <div style={bodyLayout}>
+      <div>
+        <h1>Community</h1>
+      </div>
+      <div>
+        <h3>Our Community Initiatives</h3>
+        <div style={communityStyles}>
+          <div style={communityItemStyles}>
+            <h5>LFX</h5>
+          </div>
+          <div style={communityItemStyles}>
+            <h5>GSoD</h5>
+          </div>
+          <div style={communityItemStyles}>
+            <h5>Outreachy</h5>
+          </div>
         </div>
-    )
-}
+      </div>
+      <section id='involvment'>
+          <h3>Get Involved</h3>
+          <div style={communityStyles}>
+              <div style={communityItemStyles}>
+                  <h5>Github</h5>
+              </div>
+              <div style={communityItemStyles}>
+                  <h5>Slack</h5>
+              </div>
+              <div style={communityItemStyles}>
+                  <h5>Meetings</h5>
+              </div>
+          </div>
+      </section>
+    </div>
+  );
+};
 
-export default Community
+const bodyLayout = {
+  display: 'flex',
+  flexDirection: 'column',
+  maxWidth: '1280px',
+  textAlign: 'center',
+};
+
+const communityStyles = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  alignContent: 'center',
+};
+
+const communityItemStyles = {
+  width: '33.3%',
+};
+
+export default Community;
