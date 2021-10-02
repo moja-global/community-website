5cafa9137099f7e842f9bd0d0d3d95e4b991692a
sarthakkundra <sarthakkundra21@gmail.com>
2021-10-02 12:45:16 +0530

    started homepage redesign
    Signed-off-by: sarthakkundra <sarthakkundra21@gmail.com>

--- website/src/components/HomepageFeatures.js
+++ website/src/components/HomepageFeatures.js
@@ -52,7 +52,7 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <div className="col">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
