a5d6a9409411f4bd3b7adb9ea3763d3ae5a3bea2
Mizanali Panjwani <mizanalip786@gmail.com>
2021-10-02 22:13:40 +0530

    restyle homepage
    Signed-off-by: sarthakkundra <sarthakkundra21@gmail.com>

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -10,14 +10,14 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+      <div className={clsx('container', styles.heroContainer)}>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/test">
-            Learn More 
+            Learn More
           </Link>
         </div>
       </div>
