b15b795690a2f695bedb3b32c704f4e996ba75ce
Maku <eemike07@gmail.com>
2021-10-02 13:02:29 -0400

    Fixes sitemap config; Adds robots.txt

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   title: 'Moja Global Community',
   tagline: `Learn about moja global's projects and join our ever-growing community`,
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://community.moja.global',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -129,6 +129,11 @@ module.exports = {
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+        },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          trailingSlash: false,
         },
       },
     ],
