f5008cfbc23d15fe175a509949fcd37b93e6313e
Mizanali Panjwani <mizanalip786@gmail.com>
2021-10-04 03:37:50 +0530

    added community page

--- website/communityData/meetings.js
+++ website/communityData/meetings.js
@@ -1,9 +1,19 @@
 export const pastMeetings = [
   new MeetingConstrcutor('test 1', '29-07-2021', 'https://www.google.com'),
+  new MeetingConstrcutor('test 2', '25-07-2021', 'https://www.google.com'),
+  new MeetingConstrcutor('test 3', '29-07-2021', 'https://www.google.com'),
+  new MeetingConstrcutor('test 4', '25-06-2021', 'https://www.google.com'),
+  new MeetingConstrcutor('test 5', '29-06-2021', 'https://www.google.com'),
+  new MeetingConstrcutor('test 6', '25-05-2021', 'https://www.google.com'),
 ];
 
 export const upcomingMeetings = [
-  new MeetingConstrcutor('test 2', '19-09-2019', 'https://www.moja.global', 'Harsh Bardhan'),
+  new MeetingConstrcutor('test 7', '19-09-2019', 'https://www.moja.global', 'Harsh Bardhan'),
+  new MeetingConstrcutor('test 8', '29-09-2019', 'https://www.moja.global', 'Harsh Bardhan'),
+  new MeetingConstrcutor('test 9', '19-10-2019', 'https://www.moja.global', 'Harsh Bardhan'),
+  new MeetingConstrcutor('test 10', '29-11-2019', 'https://www.moja.global', 'Harsh Bardhan'),
+  new MeetingConstrcutor('test 11', '19-12-2019', 'https://www.moja.global', 'Harsh Bardhan'),
+  new MeetingConstrcutor('test 12', '29-12-2019', 'https://www.moja.global', 'Harsh Bardhan'),
 ];
 
 function MeetingConstrcutor(agenda, dateHeld, notes, chairedBy = 'moja community') {
--- website/src/pages/community.js
+++ website/src/pages/community.js
@@ -1,59 +1,75 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
+import Layout from '@theme/Layout';
+import styles from './community.module.css';
+import CommunityCard from '../components/CommunityCard';
+import { FixedSizeList as List } from 'react-window';
+import MeetingCard from '../components/PastMeetingCard';
+import { pastMeetings, upcomingMeetings } from '../../communityData/meetings';
 
 const Community = () => {
   return (
-    <div style={bodyLayout}>
-      <div>
-        <h1>Community</h1>
-      </div>
-      <div>
-        <h3>Our Community Initiatives</h3>
-        <div style={communityStyles}>
-          <div style={communityItemStyles}>
-            <h5>LFX</h5>
-          </div>
-          <div style={communityItemStyles}>
-            <h5>GSoD</h5>
-          </div>
-          <div style={communityItemStyles}>
-            <h5>Outreachy</h5>
-          </div>
+    <Layout title='Community'>
+      <div className={styles.communityBody}>
+        <div>
+          <h1 className={styles.communityTitle}>Community</h1>
         </div>
-      </div>
-      <section id='involvment'>
-          <h3>Get Involved</h3>
-          <div style={communityStyles}>
-              <div style={communityItemStyles}>
-                  <h5>Github</h5>
-              </div>
-              <div style={communityItemStyles}>
-                  <h5>Slack</h5>
-              </div>
-              <div style={communityItemStyles}>
-                  <h5>Meetings</h5>
-              </div>
+        <section id='initiatives'>
+          <h3 className={styles.communitySubtitle}>Our Community Initiatives</h3>
+          <div className={styles.communityRow}>
+            <div className={styles.communityItem}>
+              <CommunityCard title='LFX' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed odio tellus. Aliquam ac velit et mauris rhoncus pharetra.' btnText='Click Here' />
+            </div>
+            <div className={styles.communityItem}>
+              <CommunityCard title='GSoD' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed odio tellus. Aliquam ac velit et mauris rhoncus pharetra.' btnText='Click Here' />
+            </div>
+            <div className={styles.communityItem}>
+              <CommunityCard title='Outreach' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed odio tellus. Aliquam ac velit et mauris rhoncus pharetra.' btnText='Click Here' />
+            </div>
           </div>
-      </section>
-    </div>
+        </section>
+        <section id='involvement'>
+          <h3 className={styles.communitySubtitle}>Get Involved</h3>
+          <div className={styles.communityRow}>
+            <div className={styles.communityItem}>
+              <CommunityCard title='GitHub' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed odio tellus. Aliquam ac velit et mauris rhoncus pharetra.' btnText='Click Here' />
+            </div>
+            <div className={styles.communityItem}>
+              <CommunityCard title='Slack' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed odio tellus. Aliquam ac velit et mauris rhoncus pharetra.' btnText='Click Here' />
+            </div>
+          </div>
+        </section>
+        <section id='meetings'>
+          <h3 className={styles.communitySubtitle}>Our Meetings</h3>
+          <div className={styles.communityRow}>
+            <div className={styles.communityMeeting}>
+              <h5 className={styles.meetingTitle}>Past Meetings</h5>
+              <List
+                height={500}
+                itemCount={pastMeetings.length}
+                itemSize={100}
+                width="90%"
+                itemData={{ isCompleted: true }}
+              >
+                {MeetingCard}
+              </List>
+            </div>
+            <div className={styles.communityMeeting}>
+              <h5 className={styles.meetingTitle}>Upcoming Meetings</h5>
+              <List
+                height={500}
+                itemCount={upcomingMeetings.length}
+                itemSize={100}
+                width="90%"
+                itemData={{ isCompleted: false }}
+              >
+                {MeetingCard}
+              </List>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Layout>
   );
-};
-
-const bodyLayout = {
-  display: 'flex',
-  flexDirection: 'column',
-  maxWidth: '1280px',
-  textAlign: 'center',
-};
-
-const communityStyles = {
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'flex-start',
-  alignContent: 'center',
-};
-
-const communityItemStyles = {
-  width: '33.3%',
 };
 
 export default Community;
