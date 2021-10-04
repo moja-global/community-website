27c8c4a7c3e716136e8cd00c07553741606abc5d
harshcasper <hmishra@redhat.com>
2021-10-04 11:21:58 +0530

    CHORE: Refactor docs projects to confirm to sidebar standards
    Signed-off-by: harshcasper <hmishra@redhat.com>

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/test">
+            to="/docs/Introduction/">
             Learn More
           </Link>
         </div>
