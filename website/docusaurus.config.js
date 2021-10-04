b0bfeaac826959a450c4a2144d9b42d3532687a7
Ishika414 <78595001+Ishika414@users.noreply.github.com>
2021-10-04 10:35:06 +0530

    FEAT: Enhance the Community Website Footer (#83)
    * Updated Footer
    
    Signed-off-by: ishika <raiishika414@gmail.comgit config --global user.name>
    
    * Update docusaurus.config.js
    
    * Update docusaurus.config.js
    
    * Update docusaurus.config.js
    
    Co-authored-by: ishika <raiishika414@gmail.comgit config --global user.name>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -4,7 +4,7 @@ module.exports = {
   tagline: `Learn about moja global's projects and join our ever-growing community`,
   url: 'https://community.moja.global',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'moja global', // Usually your GitHub org/user name.
@@ -44,8 +44,28 @@ module.exports = {
           title: 'Docs',
           items: [
             {
-              label: 'Projects',
-              to: '/docs/test',
+              label: 'FLINT',
+              href: 'https://moja.global/flint/',
+            },
+            {
+              label: 'FLINT UI',
+              href: 'https://docs.moja.global/projects/flint-ui/en/latest/',
+            },
+            {
+              label: 'FLINT Reporting Tool',
+              href: 'https://docs.moja.global/projects/flint-reporting/en/latest/',
+            },
+            {
+              label: 'Taswira',
+              href: 'https://github.com/moja-global/GCBM.Visualisation_Tool',
+            },
+            {
+              label: 'FLINT JSON Editor',
+              href: 'https://github.com/moja-global/FLINT.JSON_Interface',
+            },
+            {
+              label: 'FLINT Cloud',
+              href: 'https://docs.moja.global/projects/flintcloud/en/latest',
             },
           ],
         },
@@ -53,25 +73,56 @@ module.exports = {
           title: 'Community',
           items: [
             {
+              label: 'Blogs',
+              href: 'https://community.moja.global/blog',
+            },
+            {
+              label: 'News',
+              href: 'https://moja.global/news/',
+            },
+            {
+              label: 'Programs',
+              href: 'https://docs.moja.global/en/latest/contact.html#outreach-and-student-programs',
+            },
+            {
+              label: 'FAQs',
+              href: 'https://docs.moja.global/en/latest/faq.html',
+            },
+            {
               label: 'Slack',
               href: 'https://join.slack.com/t/mojaglobal/shared_invite/zt-o6ta1ug0-rVLjAo460~d7JbZ~HpFFtw',
             },
+            
+            
+          ],
+        },
+        {
+          title: 'Getting Started',
+          items: [
             {
-              label: 'Official Website',
-              href: 'https://moja.global',
+              label: 'What is FLINT?',
+              href: 'https://moja.global/flint/',
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/mojaglobal',
+              label: 'About moja global',
+              href: 'https://github.com/moja-global',
+            },
+            {
+              label: 'Newsletter',
+              href: ' https://global.us13.list-manage.com/subscribe?u=d8a187fa0dfae0bafe4bc69cc&id=cbfdc2d476',
             },
           ],
         },
         {
-          title: 'More',
+          title: 'Social',
           items: [
             {
-              label: 'Blogs',
-              to: '/blog',
+              label: 'Twitter ',
+              href: 'https://twitter.com/mojaglobal',
+            },
+             {
+              label: 'Youtube ',
+              href: 'https://www.youtube.com/channel/UCfQUrrNP1Xf-Fv4c8uHYXhQ',
             },
             {
               label: 'GitHub',
