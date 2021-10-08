aa4f1fbe7f0a92069b0cad806b96ae861b03c565
harshcasper <hmishra@redhat.com>
2021-10-08 22:49:37 +0530

    Add know the community section to the index page
    Signed-off-by: Ruchip16 <ruchipakhle@gmail.com>

--- website/src/components/HomepageFeatures.js
+++ website/src/components/HomepageFeatures.js
@@ -40,28 +40,33 @@ const FeatureList = [
 
 function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4', styles.featureRow)}>
-      <div className={styles.featureImage}>
-        <Svg className={styles.featureSvg} alt={title} />
+    <>
+      <div className={clsx('col col--4', styles.featureRow)}>
+        <div className={styles.featureImage}>
+          <Svg className={styles.featureSvg} alt={title} />
+        </div>
+        <div className={styles.featureText}>
+          <h3 className={styles.featureTitle}>{title}</h3>
+          <p className={styles.featureDescription}>{description}</p>
+        </div>
       </div>
-      <div className={styles.featureText}>
-        <h3 className={styles.featureTitle}>{title}</h3>
-        <p className={styles.featureDescription}>{description}</p>
-      </div>
-    </div>
+    </>
   );
 }
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="col">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+    <>
+      <h1 align="center">Engage with Community</h1>
+      <section className={styles.features}>
+        <div className="container">
+          <div className="col">
+            {FeatureList.map((props, idx) => (
+              <Feature key={idx} {...props} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
