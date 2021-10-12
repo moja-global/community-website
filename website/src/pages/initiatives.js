8cf870a279e8b66fcd489689eb1fba16d8216856
Harsh Mishra <erbeusgriffincasper@gmail.com>
2021-10-12 14:19:47 +0530

    Update initiatives.js

--- website/src/pages/initiatives.js
+++ website/src/pages/initiatives.js
@@ -57,7 +57,7 @@ const Community = () => {
                 title="GitHub"
                 description="We have a ton of open issues which could use your help and expertise. "
                 btnText="Learn More"
-                btnLink="github.com/moja-global"
+                btnLink="https://github.com/moja-global"
               />
             </div>
             <div className={styles.communityItem}>
@@ -65,7 +65,7 @@ const Community = () => {
                 title="Slack"
                 description="We would love to know you better. Hop on and chat with the community."
                 btnText="Join Us"
-                btnLink="join.slack.com/t/mojaglobal/shared_invite/zt-o6ta1ug0-rVLjAo460~d7JbZ~HpFFtw"
+                btnLink="https://join.slack.com/t/mojaglobal/shared_invite/zt-o6ta1ug0-rVLjAo460~d7JbZ~HpFFtw"
               />
             </div>
           </div>
