9d4175c90db296107e7629c108107f0b90bfa86b
Anirudh Panda <anirudhpandaofficial@gmail.com>
2021-10-13 11:29:02 +0530

    DOCS: Add Strategic Plan for moja global (#149)
    * fixed button color on light theme fixes #97
    
    Signed-off-by: Anirudh Panda <anirudhpandaofficial@gmail.com>
    
    * customised light colors for dark mode
    
    * docs: added Strategic Plan Change

--- website/sidebarsCommunity.js
+++ website/sidebarsCommunity.js
@@ -1,15 +1,15 @@
-
 module.exports = {
-    community: [
-        'support',
-        'technical-steering-committee',
-        'governance',
-        'contributing',
-        'community-website-contributions',
-        'code-contribution',
-        'docs-contribution',
-        'management-contribution',
-        'mentorship',
-        'code-of-conduct',
-    ]
-}
+  community: [
+    'support',
+    'technical-steering-committee',
+    'governance',
+    'contributing',
+    'community-website-contributions',
+    'code-contribution',
+    'docs-contribution',
+    'management-contribution',
+    'mentorship',
+    'strategic-plan-change',
+    'code-of-conduct',
+  ],
+};
