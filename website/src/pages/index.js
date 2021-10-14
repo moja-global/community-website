858ef032e95ec155f1a7d0ee025e4d55e0a8760e
Anirudh Panda <anirudhpandaofficial@gmail.com>
2021-10-14 11:01:28 +0530

    updated homepage title

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -288,11 +288,11 @@ function Feature({ title, description }) {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
+  // const {  } = useDocusaurusContext();
   return (
     <>
       <Layout
-        title={`Hello from ${siteConfig.title}`}
+        // title={`Hello from ${siteConfig.title}`}
         description="Description will go into a meta tag in <head />"
       >
         <HomepageHeader />
