6413508b9f8f581974d786933a1fedce68d67809
Sukriti-sood <soodsukriti1111@gmail.com>
2021-10-21 21:23:01 +0530

    made changes
    Signed-off-by: Sukriti-sood <soodsukriti1111@gmail.com>

--- website/src/components/SponsorAvatar/index.js
+++ website/src/components/SponsorAvatar/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 const SponsorAvatar = ({ name, logo, url }) => {
   return (
     <a title={name} target="_blank" href={url} rel="noreferrer">
-      <div className="sponsor-div">
-        <img className="sponsor-avatar" src={logo} alt={name} />
-      </div>
+      <div className="sponsor_wrapper">
+        <img width="120" height="120" className="sponsor_avatar" src={logo} alt={name} />{' '}
+      </div>{' '}
     </a>
   );
 };
--- website/src/components/Sponsors/index.js
+++ website/src/components/Sponsors/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import sponsors from '../../sponsors.json';
+import sponsors from '../../../websiteData/sponsors.json';
 import LazyLoad from 'react-lazyload';
 import SponsorAvatar from '../SponsorAvatar';
 
-const Sponsors = (props) => {
+const Sponsors = () => {
   return (
     <div className="sponsors">
       <div className="pluginsHeader">Sponsors</div>
       <div>
         {sponsors.length > 0 && (
           <LazyLoad height={200}>
-            <div className="sponsor-content">
+            <div className="sponsor_content">
               {sponsors.map((data, i) => (
                 <SponsorAvatar key={i} {...data} />
               ))}
