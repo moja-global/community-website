562d036e1111788649b6ad0307159830a1db4763
asmi jafar <asmijafar20@gmail.com>
2021-10-23 21:28:34 +0530

    fix lint issues and make changes
    Signed-off-by: asmi jafar <asmijafar20@gmail.com>

--- website/src/pages/TeamCard/teamcard.js
+++ website/src/pages/TeamCard/teamcard.js
@@ -1,42 +1,35 @@
-import React from 'react'
+import React from 'react';
 import JsonData from './all-contributors-data.json';
-import styles from './teamcard.css';
-import Card from "react-bootstrap/Card";
+import './teamcard.css';
+import Card from 'react-bootstrap/Card';
 
+const JsonDataDisplay = () => {
+  const DisplayData = JsonData.map((info) => {
+    return (
+      <div className="card-list" key={info.name}>
+        <div className="card-container">
+          <Card>
+            <Card.Img className="avatar__photo avatar__photo--xl" src={info.avatar_url} />
+            <Card.Body>
+              <Card.Title>{info.name}</Card.Title>
+              <Card.Subtitle>
+                Profile: <a href={info.profile}>{info.github_username}</a>
+              </Card.Subtitle>
+              <Card.Text>Projects Contributed to:</Card.Text>
 
- const JsonDataDisplay = () => {
-    const DisplayData=JsonData.map(
-      (info)=>{
-        return(
-        <div className="card-list">
-          <div className="card-container">
-            <Card>
-              <Card.Img className="avatar__photo avatar__photo--xl" src={info.avatar_url}/>
-              <Card.Body>
-                <Card.Title>{info.name}</Card.Title>
-                <Card.Subtitle>
-                  Profile: <a href={info.profile}>{info.github_username}</a>
-                </Card.Subtitle>
-                <Card.Text>
-                  Projects Contributed to:
-                  </Card.Text>
-                
-                   
-                  {info.projects.map(item => <label className="btn">{item}</label>)}
-                    
-                </Card.Body>
-            </Card>
-          </div>
+              {info.projects.map((item) => (
+                <label className="btn" key={info.name}>
+                  {item}
+                </label>
+              ))}
+            </Card.Body>
+          </Card>
         </div>
-              )
-              }
-                )
- 
-    return(
-    <div className="card-list">
-      {DisplayData}
       </div>
-      )
-    }
- 
- export default JsonDataDisplay;
+    );
+  });
+
+  return <div className="card-list">{DisplayData}</div>;
+};
+
+export default JsonDataDisplay;
--- website/src/pages/team.js
+++ website/src/pages/team.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import JsonDataDisplay from './TeamCard/teamcard';
-import styles from './team.css';
+import './team.css';
 
-  const Team = () => {
+const Team = () => {
   return (
     <Layout>
-    <div className="wrapper">
-      <h1>Contributors</h1>
-      <div class="subtitle">
-      Thanks to everyone involved in Contributing to The Moja-Global.
-      </div>
-      
-    <div className="card-list">
-    <div className="card-container">
+      <div className="wrapper">
+        <h1>Contributors</h1>
+        <div className="subtitle">
+          Thanks to everyone involved in Contributing to The Moja-Global.
+        </div>
 
-      <JsonDataDisplay/>
-    </div>
-    </div>
-    </div>
+        <div className="card-list">
+          <div className="card-container">
+            <JsonDataDisplay />
+          </div>
+        </div>
+      </div>
     </Layout>
   );
-}
- 
+};
+
 export default Team;
