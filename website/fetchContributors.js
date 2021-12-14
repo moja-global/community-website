997e4ba77eb4c27f1ec22ca3a22893bda769bfc6
Sukriti-sood <soodsukriti1111@gmail.com>
2021-12-14 12:49:20 +0530

    modeified script

--- website/fetchContributors.js
+++ website/fetchContributors.js
@@ -1,62 +1,86 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const request = require('request');
+// const request = require('request');
 const path = require('path');
 
-const REQUIRED_KEYS = ['total', 'author'];
+// const REQUIRED_KEYS = ['total', 'author'];
 
-const BLACKLIST = [
-];
+// const BLACKLIST = [];
 
-request(
-  {
-    url: 'https://api.github.com/repos/moja-global/community-website/stats/contributors',
-    headers: {
-      'User-Agent': 'request contributors for moja global',
-    },
-  },
-  (err, response, body) => {
-    if (err) console.error('Failed to fetch contributors: ', err);
+const axios = require('axios');
 
-    // Basic validation
-    let content = JSON.parse(body);
 
-    // FIXME: if (!Array.isArray(content)) throw new Error('contributors info is not an array');
+(async() => {
+    let page = 1;
+    const repoArray = [];
 
-    for (const item of content) {
-      for (const key of REQUIRED_KEYS) {
-        if (!item || typeof item !== 'object')
-          throw new Error(`contributors info item (${JSON.stringify(item)} is not an object`);
-        if (!(key in item))
-          throw new Error(`contributors info item (${JSON.stringify(item)} doesn't include ${key}`);
-      }
-      if (item.weeks) {
-        lastContribution = 0;
-        if (Array.isArray(item.weeks)) {
-          item.weeks.forEach((data) => {
-            if (data.a + data.d + data.c > 0 && data.w > lastContribution) {
-              lastContribution = data.w;
-            }
-          });
+    while (1) {
+        const res = await axios(
+            'https://api.github.com/orgs/moja-global/repos?per_page=100&page=' + page
+        );
+
+        const repos = res.data;
+
+        for (const repo of repos) {
+            const repoInfo = { name: repo.name, contribUrl: repo.contributors_url, url: repo.html_url };
+
+            repoArray.push(repoInfo);
         }
-        item.lastContribution = lastContribution;
-        delete item.weeks;
-      }
+
+        if (repos.length === 0) {
+            break;
+        }
+
+        page++;
     }
 
-    content = content.filter((item) => {
-      return !(item.author && (BLACKLIST.includes(item.author.id) || item.author.type === 'Bot'));
-    });
+    const contributorsInfo = [];
+
+    function checkAlreadyExist(username, contributorsInfo) {
+        for (let i = 0; i < contributorsInfo.length; i++) {
+            if (contributorsInfo[i].username === username) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    for (const repo of repoArray) {
+        const res = await axios(repo.contribUrl);
+
+        const contributors = res.data;
+
+        const projectInfo = { projectName: repo.name, projectUrl: repo.url };
+
+        for (const constributor of contributors) {
+            const username = constributor.login;
+
+            const contributorIndex = checkAlreadyExist(username, contributorsInfo);
+
+            if (contributorIndex === -1) {
+                const contributorInfo = {
+                    username: username,
+                    photoUrl: constributor.avatar_url,
+                    url: constributor.html_url,
+                    projects: [projectInfo],
+                };
+
+                contributorsInfo.push(contributorInfo);
+            } else {
+                contributorsInfo[contributorIndex].projects.push(projectInfo);
+            }
+        }
+    }
 
     fs.writeFile(
-      path.resolve(__dirname, 'src/contributors.json'),
-      JSON.stringify(content, null, 2) + '\n',
-      (err) => {
-        if (err) {
-          console.error('Failed to write contributors file: ', err);
-        } else console.log('Fetched 1 file: contributors.json');
-      }
+        path.resolve(__dirname, 'src/contributors.json'),
+        JSON.stringify(contributorsInfo, null, 2) + '\n',
+        (err) => {
+            if (err) {
+                console.error('Failed to write contributors file: ', err);
+            } else console.log('Fetched 1 file: contributors.json');
+        }
     );
-  }
-);
+})();
