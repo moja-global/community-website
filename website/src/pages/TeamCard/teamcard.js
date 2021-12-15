4ef2878a42546fcfd21501af9d61db389f20e6ff
Sukriti-sood <soodsukriti1111@gmail.com>
2021-12-15 19:57:43 +0530

    fix linting
    Signed-off-by: Sukriti-sood <soodsukriti1111@gmail.com>

--- website/src/pages/TeamCard/teamcard.js
+++ website/src/pages/TeamCard/teamcard.js
@@ -1,29 +1,42 @@
 import React from 'react';
-import JsonData from '../../contributors.json'
+import JsonData from '../../contributors.json';
 import styles from './teamcard.module.css';
 
-
-const JsonDataDisplay = ()  => {
-  
+const JsonDataDisplay = () => {
   const DisplayData = JsonData.map((info) => {
-
     return (
-           
-            <div className={styles.card}>
-             <a  href={info.url}  ><img height="23px" width="32px" alt='github-Profile' className="avatar__photo avatar__photo--xl" src={info.photoUrl} /></a> 
-              <a href={info.url}><h3 className={styles.username} >{info.username}</h3></a>
-              <p>Projects Contributed to:  </p>
-              <p className={styles.projects}>
-              {info.projects.map((item) => (
-               <a  href ={item.projectUrl}> <p className={styles.item}><label className={styles.button} key={info.username}>
-                  {item.projectName}
-                </label></p></a>
-              ))}
-              </p>
-               </div>
+      <div key={info.username} className={styles.card}>
+        <a href={info.url}>
+          <img
+            height="23px"
+            width="32px"
+            alt="github-Profile"
+            className="avatar__photo avatar__photo--xl"
+            src={info.photoUrl}
+          />{' '}
+        </a>{' '}
+        <a href={info.url}>
+          <h3 className={styles.username}> {info.username} </h3>{' '}
+        </a>{' '}
+        <p> Projects Contributed to: </p>{' '}
+        <p className={styles.projects}>
+          {' '}
+          {info.projects.map((item) => (
+            <a key={info.username} href={item.projectUrl}>
+              {' '}
+              <p className={styles.item}>
+                <label className={styles.button} key={info.username}>
+                  {' '}
+                  {item.projectName}{' '}
+                </label>{' '}
+              </p>{' '}
+            </a>
+          ))}{' '}
+        </p>{' '}
+      </div>
     );
   });
-  return <div className={styles.projectContainer}>{DisplayData}</div>;
+  return <div className={styles.projectContainer}> {DisplayData} </div>;
 };
 
 export default JsonDataDisplay;
--- website/src/pages/team.js
+++ website/src/pages/team.js
@@ -4,19 +4,17 @@ import JsonDataDisplay from './TeamCard/teamcard';
 import styles from './team.module.css';
 
 const Team = () => {
-  
   return (
     <Layout>
       <div className={styles.communityBody}>
-
         <h1 className={styles.communityTitle}>Contributors</h1>
 
-          <h2 className={styles.communitySubtitle}>
+        <h2 className={styles.communitySubtitle}>
           Thanks to everyone involved in Contributing to The Moja-Global.
-            </h2>
-         
+        </h2>
+
         <JsonDataDisplay />
-        </div>
+      </div>
     </Layout>
   );
 };
