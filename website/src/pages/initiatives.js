2aaa4554f81a2289ce89437231fed9b97ee04544
sohamsshah <sohamshah456@gmail.com>
2022-02-13 00:04:58 +0530

    fix: image links and test build

--- website/src/pages/initiatives.js
+++ website/src/pages/initiatives.js
@@ -5,6 +5,12 @@ import CommunityCard from '../components/CommunityCard';
 import { FixedSizeList as List } from 'react-window';
 import MeetingCard from '../components/PastMeetingCard';
 import { pastMeetings, upcomingMeetings } from '../../websiteData/meetings';
+import LFXLogo from './../../static/img/linux.png';
+import GSODLogo from './../../static/img/gsod.webp';
+import GSOCLogo from './../../static/img/gsoc.png';
+import OutreachyLogo from './../../static/img/Outreach.png';
+import GithubLogo from './../../static/img/github.png';
+import SlackLogo from './../../static/img/slack.png';
 
 const Community = () => {
   return (
@@ -18,37 +24,29 @@ const Community = () => {
             <div className={styles.communityItem}>
               <CommunityCard
                 title="Linux Foundation Mentorship"
-                image="static\img\linux.png"
-                description="The Linux Foundation is the parent organisation of moja global. Therefore, we actively participate and host our projects for the LFX Mentorship programme."
+                image={LFXLogo}
                 btnLink="/community/mentorship#linux-foundation-mentorship/"
-                btnText="Learn More"
               />
             </div>
             <div className={styles.communityItem}>
               <CommunityCard
                 title="Google Season of Docs"
-                image="static\img\gsod.webp"
-                description="Moja global participates in Google Season of Docs. We love technical writers who can bring in clarity about as well as desrcibe projects with good as well as oriented documentation"
+                image={GSODLogo}
                 btnLink="/community/mentorship#google-season-of-docs/"
-                btnText="Learn More"
               />
             </div>
             <div className={styles.communityItem}>
               <CommunityCard
                 title="Outreachy Internships"
-                image="static\img\Outreach.png"
-                description="Moja global participates in Outreachy. We love to support under-represented groups in technology who can help accelerate our project growth."
+                image={OutreachyLogo}
                 btnLink="/community/mentorship#outreachy-internships/"
-                btnText="Learn More"
               />
             </div>
             <div className={styles.communityItem}>
               <CommunityCard
                 title="Google Summer of Code"
-                image="static\img\gsoc.svg"
-                description="Moja global participates in Google Summer of Code. We love to host a wide range of contributors to help us accelerate our project growth."
+                image={GSOCLogo}
                 btnLink="/community/mentorship#google-summer-of-code/"
-                btnText="Learn More"
               />
             </div>
           </div>
@@ -59,18 +57,14 @@ const Community = () => {
             <div className={styles.communityItem}>
               <CommunityCard
                 title="GitHub"
-                image="static\img\github.png"
-                description="We have a ton of open issues which could use your help and expertise. "
-                btnText="Learn More"
+                image={GithubLogo}
                 btnLink="https://github.com/moja-global"
               />
             </div>
             <div className={styles.communityItem}>
               <CommunityCard
                 title="Slack"
-                image="static\img\slack.png"
-                description="We would love to know you better. Hop on and chat with the community."
-                btnText="Join Us"
+                image={SlackLogo}
                 btnLink="https://join.slack.com/t/mojaglobal/shared_invite/zt-o6ta1ug0-rVLjAo460~d7JbZ~HpFFtw"
               />
             </div>
