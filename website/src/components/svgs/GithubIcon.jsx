5387e7f1f3693260047e19da0fd28d29a0f922f7
ayush78781 <ayush70781@gmail.com>
2022-06-07 01:51:39 +0530

    Chnaged Roadmaps container Icons color & added target attribute in socials of community(footer)

--- website/src/components/svgs/GithubIcon.jsx
+++ website/src/components/svgs/GithubIcon.jsx
@@ -6,7 +6,7 @@ function GithubIcon({ ...props }) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="blue"
+      stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
